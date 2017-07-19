d7ee3ed993cc82b7df23e4f3abf126d7efdd177e
John Markhorst <jmarkhorst@comcast.net>
2017-07-19 18:58:45 -0400

    test

--- AppServer/util/copytowebtest.bat
+++
@@ -1,5 +0,0 @@
-xcopy C:\workspace\DVData\AppServer\po\*.* T:\WebRestDVData\po\ /d /r /e /y
-xcopy C:\workspace\DVData\AppServer\wh\*.* T:\WebRestDVData\wh\ /d /r /e /y
-xcopy C:\workspace\DVData\AppServer\util\*.* T:\WebRestDVData\util\ /d /r /e /y
-xcopy C:\workspace\DVData\AppServer\rf\*.* T:\WebRestDVData\rf\ /d /r /e /y
-xcopy C:\workspace\DVData\AppServer\*.* T:\WebRestDVData\ /d /r /e /y
